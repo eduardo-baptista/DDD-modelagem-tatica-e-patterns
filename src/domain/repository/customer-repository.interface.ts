e8616a7d2c6dd2220e21812941dd2adc494cfd60
Eduardo Baptista <eduardofbaptista@gmail.com>
2024-02-13 11:21:00 -0300

    Split modules

--- src/domain/repository/customer-repository.interface.ts
+++
@@ -1,5 +0,0 @@
-import type { Customer } from "../entity/customer";
-import type { RepositoryInterface } from "./repository.interface";
-
-export interface CustomerRepositoryInterface
-	extends RepositoryInterface<Customer> {}
--- src/domain/repository/order-repository.interface.ts
+++
@@ -1,4 +0,0 @@
-import type { Order } from "../entity/order";
-import type { RepositoryInterface } from "./repository.interface";
-
-export interface OrderRepositoryInterface extends RepositoryInterface<Order> {}
